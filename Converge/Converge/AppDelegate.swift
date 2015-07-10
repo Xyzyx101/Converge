95370251106287100615082a2d436d820b859a7c
Andrew Perrault <andrew.perrault@yahoo.ca>
2015-07-10 11:41:35 -0400

    Removed some debug code

--- Converge/Converge/AppDelegate.swift
+++ Converge/Converge/AppDelegate.swift
@@ -15,34 +15,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        println("didFinishLoading")
-        // Override point for customization after application launch.
         return true
     }
 
     func applicationWillResignActive(application: UIApplication) {
-        println("resignActive")
-        //SKTAudio.sharedInstance().backgroundMusicPlayer?.stop()
-        //SKTAudio.sharedInstance().soundEffectPlayer?.stop()
+        
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        println("EnterBackground")
         Audio.instance().musicOff()
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
-        println("EnterForeground")
         Audio.instance().musicOn()
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
-        println("Active")
         Audio.instance().playMusic()
     }
 
     func applicationWillTerminate(application: UIApplication) {
-        println("terminate")
         SKTAudio.sharedInstance().backgroundMusicPlayer?.stop()
         SKTAudio.sharedInstance().soundEffectPlayer?.stop()
     }
